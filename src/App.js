9ad7ad8e259d21d8347f9b56e93d94bd783c5c98
Eddie Mc <eddiemc.tech@gmail.com>
2025-04-24 13:24:55 +0300

    Update App.js to add chatbot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Gallery from "./Components/Gallery";
 import About from "./Components/About";
 import CustomPage from "./Components/CustomPage";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import Chatbot from "./Components/Chatbot"; // <-- Add this line
 import "./App.css";
 
 function App() {
@@ -14,13 +15,13 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          {/* Define routes here */}
           <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/about" element={<About />} />
           <Route path="/custom" element={<CustomPage />} />
         </Routes>
         <Footer />
+        <Chatbot /> {/* <-- Add this here */}
       </div>
     </Router>
   );
